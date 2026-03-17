4c192f984dcb4e4ad4820c5a995aded70e0ea6db
felipcocev <joao@aideia.com>
2026-03-17 09:50:25 -0300

    Update kanban.routes.js

--- app/javascript/dashboard/routes/dashboard/kanban/kanban.routes.js
+++ app/javascript/dashboard/routes/dashboard/kanban/kanban.routes.js
@@ -1,21 +1,9 @@
-import { frontendURL } from '../../../helper/URLHelper';
 import KanbanIndex from '../../../views/kanban/Index.vue';
-
-const CONVERSATION_PERMISSIONS = [
-  'administrator',
-  'agent',
-  'conversation_manage',
-  'conversation_unassigned_manage',
-  'conversation_participating_manage',
-];
 
 export const routes = [
   {
     path: 'kanban',
     name: 'kanban_dashboard',
-    meta: {
-      permissions: CONVERSATION_PERMISSIONS,
-    },
     component: KanbanIndex,
   },
 ];
